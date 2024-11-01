043a0a547622c236e3f99e60b81dc894463d8c9a
Arina <GagarinaArina745@ya.ru>
2024-11-01 23:14:22 +0300

    Update constant.php

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+
+const myPI = 3.14;
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Константы</title>
+</head>
+
+<body>
+    <h1>Константы</h1>
+    <?php
+    /*
+       ЗАДАНИЕ 2
+       - Проверьте, существует ли константа, которую Вы хотите использовать.
+       - Выведите значение созданной константы.
+       ЗАДАНИЕ 3
+       - Используя предопределённые в ядре константы выведите текущую версию PHP.
+       - Используя магические константы выведите директорию скрипта.
+       */
+
+    ?>
+</body>
+
+</html>
