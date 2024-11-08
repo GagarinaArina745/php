8588f62fd08ba6beed0a9039e52157ead431eeaa
Arina <GagarinaArina745@ya.ru>
2024-11-08 23:53:14 +0300

    Update table.php

--- lab3/site/table.php
+++ lab3/site/table.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,12 @@
   <title>Таблица умножения</title>
   <link rel="stylesheet" href="style.css">
 </head>
+
+<?php
+include 'inc/lib.inc.php';
+include 'inc/data.inc.php';
+?>
+
 <body>
   <header>
     <!-- Верхняя часть страницы -->
@@ -31,53 +38,21 @@
       <label>Цвет: </label>
       <br>
       <input name='color' type='color' value='#ff0000' list="listColors">
-	<datalist id="listColors">
-		<option>#ff0000</option>/>
-		<option>#00ff00</option>
-		<option>#0000ff</option>
-	</datalist>
+      <datalist id="listColors">
+        <option>#ff0000</option>/>
+        <option>#00ff00</option>
+        <option>#0000ff</option>
+      </datalist>
       <br>
       <br>
       <input type='submit' value='Создать'>
     </form>
     <br>
     <!-- Таблица -->
-    <table border='1' width='200'>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>4</td>
-        <td>6</td>
-        <td>8</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>6</td>
-        <td>9</td>
-        <td>12</td>
-        <td>15</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>8</td>
-        <td>12</td>
-        <td>16</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>10</td>
-        <td>15</td>
-        <td>20</td>
-        <td>25</td>
-      </tr>
+    <table>
+      <?php
+      getTable((int) $cols, (int) $rows, $color);
+      ?>
     </table>
     <!-- Таблица -->
     <!-- Область основного контента -->
@@ -85,13 +60,11 @@
   <nav>
     <h2>Навигация по сайту</h2>
     <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a></li>
-      <li><a href='about.php'>О нас</a></li>
-      <li><a href='contact.php'>Контакты</a></li>
-      <li><a href='table.php'>Таблица умножения</a></li>
-      <li><a href='calc.php'>Калькулятор</a></li>
-    </ul>
+    <?php
+
+    getMenu($menu);
+
+    ?>
     <!-- Меню -->
   </nav>
   <footer>
@@ -100,4 +73,5 @@
     <!-- Нижняя часть страницы -->
   </footer>
 </body>
+
 </html>
