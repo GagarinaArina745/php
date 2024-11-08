3bdd2a095717260a32f1f8f7807b7531c4b83528
Arina <GagarinaArina745@ya.ru>
2024-11-08 23:59:37 +0300

    добавлен шаблон

---
+++ lab4/site/inc/lib.inc.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+
+/**
+ * Функция, рисующая таблицу умножения
+ * @param int $cols - количество столбцов таблицы,
+ * @param int $rows - количество строк таблицы,
+ * @param string $color - цвет первых столбца и строки,
+ * @return int  - количество вызывов функции.
+ */
+function getTable(int $cols = 4, int $rows = 5, string $color = 'yellow'): int
+{
+
+    static $count = 0;
+    $count++;
+
+    //echo "<table>";
+
+    echo "<tr>";
+    echo '<th style="background-color:', $color, ';">*</th>';
+
+    for ($i = 1; $i <= $cols; $i++) {
+        echo '<th style="background-color:', $color, ';">', $i, '</th>';
+    }
+    echo "</tr>";
+
+    for ($i = 1; $i <= $rows; $i++) {
+        echo "<tr>";
+
+        echo '<th style="background-color:', $color, ';">', $i, '</th>';
+        for ($j = 1; $j <= $cols; $j++) {
+            echo "<td>", $i * $j, "</td>";
+        }
+        echo "</tr>";
+    }
+    //echo "</table>";
+
+    return $count;
+}
+
+
+
+
+/**
+ * Функция, создающая меню.
+ * @param array $menu - массив,содержащий элементы меню и ссылки,
+ * @param bool $vertical - при true (по умолчанию) меню вертикальное, иначе - горизонтальное
+ */
+function getMenu(array $menu, bool $vertical = true)
+{
+    if ($vertical) {
+        $menuType = 'menu';
+    } else {
+        $menuType = 'menu novertical';
+    }
+
+
+    echo "<ul class='$menuType'>";
+    foreach ($menu as $item) {
+        echo "<li><a href=\"{$item['href']}\">{$item['link']}</a></li>";
+    }
+
+    echo "</ul>";
+
+}
