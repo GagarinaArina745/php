1933a9c36f3dbf5394a77a17aea116f692be66a3
Arina <GagarinaArina745@ya.ru>
2024-11-13 22:51:12 +0300

    Update visited.inc.php

--- lab6/session/visited.inc.php
+++ lab6/session/visited.inc.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 // Код для всех страниц - вывод информации о посещенных страницах
 
+echo "<h2>Список посещённых страниц</h2>";
 if (isset($_SESSION['visits']) && count($_SESSION['visits']) > 0) {
     
 	echo "<ul>";
