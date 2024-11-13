f187732e01e0e59ede45c544705761a815511bda
Arina <GagarinaArina745@ya.ru>
2024-11-13 22:24:16 +0300

    задание 3 лаба 5

--- lab4/site/contact.php
+++ lab4/site/contact.php
@@ -1,3 +1,32 @@
+<?php
+declare(strict_types=1);
+
+$msg = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+
+  $sendTo = " GagarinaArina745@yandex.ru";
+  $sendFrom = "admin@center.ogu";
+
+  $headers = "From: $sendFrom\r\n" .
+    "Reply-To: $sendFrom\r\n" .
+    "Content-Type: text/plain; charset=utf-8";
+
+
+  if (mail($sendTo, $subject, $body, $headers)) {
+    $msg = "<p>Сообщение отправлено.</p>";
+  }
+  else {
+    $msg = "<p>Ошибка при отправке сообщения.</p>";
+  }
+}
+?>
+
+
 <!-- Область основного контента -->
 <h3>Адрес</h3>
 <address>123456 Москва, Малый Американский переулок 21</address>
@@ -15,3 +44,5 @@
   <input type='submit' value='Отправить'>
 </form>
 <!-- Область основного контента -->
+
+<? echo $message ?>
