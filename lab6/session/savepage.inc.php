f50d72ce77d238604574e549c5b88b935b2ab100
Arina <GagarinaArina745@ya.ru>
2024-11-13 22:49:33 +0300

    2.2 2.3

--- lab6/session/savepage.inc.php
+++ lab6/session/savepage.inc.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 // Код для всех страниц - сохранение информации о посещенных страницах
 
-if (!isset($_SESSION['visitedPages'])) {
-	$_SESSION['visitedPages'] = [];
+if (!isset($_SESSION['visits'])) {
+	$_SESSION['visits'] = [];
 }
 	
 // добавляем путь в массив
-$_SESSION['visitedPages'][] = $_SERVER['REQUEST_URI'];
+$_SESSION['visits'][] = $_SERVER['REQUEST_URI'];
